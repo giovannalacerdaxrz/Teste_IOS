0c44cedee3dd705ae1432fc1951e07820032bd41
Agatha Costa <agathacosta113@gmail.com>
2022-10-26 10:42:42 -0300

    atividade array

---
+++ while.js
@@ -0,0 +1,19 @@
+// console.clear()
+// var n1 = 10
+// while(n1 > 0 ){
+
+//     console.log(`N1 vale : ${n1}`);
+//     n1 ++
+// }
+// console.log('Final do programa');
+// var n2 = 10
+
+// while
+// do {
+//     console.log(`N1 vale : ${n1}`);
+//     n1++;
+// }
+for (var viagem = 1000; viagem < 3000; viagem += 200) {
+    console.log(`Tenho ${viagem} até o momento.`);
+}
+console.log(`Cheguei em ${viagem},posso ir a viagem `);
